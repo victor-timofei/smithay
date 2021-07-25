e2c75fc57e2b30a9f17eb4a4558b6fe77b01a5b3
vberger <vberger@users.noreply.github.com>
2021-07-25 09:22:04 +0000

    deploy: 07deba4c1b08b219d1bf43dd451dcf44ce3a1746

--- wayland_protocols/unstable/xwayland_keyboard_grab/v1/sidebar-items.js
+++ wayland_protocols/unstable/xwayland_keyboard_grab/v1/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["client","Client-side API of this protocol"],["server","Server-side API of this protocol"]]});
+initSidebarItems({"mod":[["server","Server-side API of this protocol"]]});
