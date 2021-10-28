df5b2495f0fe037a2f72c9dc492caa0dd63f49f4
vberger <vberger@users.noreply.github.com>
2021-10-28 14:43:05 +0000

    deploy: 1a1fe01349b93bf3820a4d4b23c8317d3ecb889d

--- crates.js
+++ crates.js
@@ -1,1 +1,1 @@
-window.ALL_CRATES = ["calloop","dbus","drm","gbm","input","nix","slog","smithay","udev","wayland_commons","wayland_protocols","wayland_server","winit"];
+window.ALL_CRATES = ["calloop","dbus","drm","gbm","input","nix","slog","smithay","udev","wayland_commons","wayland_protocols","wayland_server","winit","x11rb"];
